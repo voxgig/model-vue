ae3e9d0f98fc1c687879cb01383a205be57341da
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-08-23 18:27:27 +0200

    use "v-for" for feature buttons

--- src/components/BasicHead.vue
+++ src/components/BasicHead.vue
@@ -107,31 +107,23 @@
     >mdi-chevron-left</v-icon>
 
   <v-divider vertical v-if="show('print')"></v-divider>
+  <li v-for="feature of featuresMenu" style="list-style-type: none;" :key="feature.title">
+    <v-btn v-if="feature.title != 'bookmark' && tool[feature.title].active && show(feature.title)"
+      large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" @click="feature.click()">
+      <v-icon large class="vxg-icon">{{feature.icon}}</v-icon>
+    </v-btn>
 
-  <v-btn v-if="tool.print.active && show('print')" 
-    large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" @click="printMap()">
-    <v-icon large class="vxg-icon">mdi-printer</v-icon>
-  </v-btn>
+    <v-btn v-if="feature.title == 'bookmark' && tool.bookmark.active && show('bookmark') && bookmarkVisible" 
+      large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" @click="feature.click()">
+      <v-icon large class="vxg-icon">{{feature.icon}}</v-icon>
+    </v-btn>
+    <v-btn v-if="feature.title == 'bookmark' && tool.bookmark.active && show('bookmark') && !bookmarkVisible" 
+      large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" disabled>
+      <v-icon large class="vxg-icon">{{feature.icon}}</v-icon>
+    </v-btn>
 
-  <v-divider vertical v-if="show('print')"></v-divider>
-
-  <v-btn v-if="tool.bookmark.active && show('bookmark') && bookmarkVisible" 
-    large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" @click="showTags()">
-    <v-icon large class="vxg-icon">mdi-bookmark-minus-outline</v-icon>
-  </v-btn>
-  <v-btn v-if="tool.bookmark.active && show('bookmark') && !bookmarkVisible" 
-    large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px" disabled>
-    <v-icon large class="vxg-icon">mdi-bookmark-minus-outline</v-icon>
-  </v-btn>
-
-  <v-divider vertical v-if="show('bookmark')"></v-divider>
-
-  <v-btn v-if="tool.collect.active && show('collect')" 
-    large elevation="0" class="pa-1 ma-1" color="white" style="height: 55px">
-    <v-icon large class="vxg-icon">mdi-folder-open-outline</v-icon>
-  </v-btn>
-
-  <v-divider vertical v-if="show('collect')"></v-divider>
+    <v-divider vertical v-if="show(feature.title)"></v-divider>
+  </li>
 
 </v-app-bar>
 </template>
@@ -181,7 +173,16 @@ export default {
     }
   },
 
-
+  created () {
+    let features = ['print', 'bookmark', 'collect']
+    for(let feature of features) {
+      this.featuresMenu.push(Object.assign({}, 
+        this.tool[feature], 
+        {title: feature, click: this[this.tool[feature].click]}))
+    }
+    // console.log('featuresMenu:  ', this.featuresMenu)
+  },
+  
   mounted () {
     if(this.tool.select.active) {
       this.select = this.tool.select.initial
